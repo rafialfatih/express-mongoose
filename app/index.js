ad456a3c3542efab40c8eb1ecf157c099b3f4684
rafialfatih <rafi.alfatih@gmail.com>
2023-05-28 18:31:53 +0700

    Seperate server from index

--- app/index.js
+++ app/index.js
@@ -1,26 +1,12 @@
 import express from 'express'
-import mongoose from 'mongoose'
-
-import config from './config/config'
 import productsRoutes from './routes/products'
 import morgan from './middlewares/morgan.middleware'
-import logger from './utils/logger'
 
 const app = express()
 
 app.use(express.json())
 app.use(morgan)
 
-const start = async () => {
-    try{
-        await mongoose.connect(config.mongodb_uri, config.option)
-        app.listen(config.port, () => logger.info(`Server is running at port ${config.port}`))
-    } catch (err) {
-        console.error(err)
-        process.exit(1)
-    }
-}
-
-start()
-
 app.use('/api/products', productsRoutes)
+
+export default app
---
+++ app/server.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+import config from './config/config'
+import logger from './utils/logger'
+
+import app from './index'
+
+const start = async () => {
+    try{
+        await mongoose.connect(config.mongodb_uri, config.option)
+        app.listen(config.port, () => logger.info(`Server is running at port ${config.port}`))
+    } catch (err) {
+        console.error(err)
+        process.exit(1)
+    }
+}
+
+start()
